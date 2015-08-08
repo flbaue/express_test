4f1a8b1e38995bc6d3234afbe43f41e04c8db050
flbaue <flbaue@users.noreply.github.com>
2015-08-08 17:05:10 +0200

    added open shift integration

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 var express = require('express');
 var app = express();
 
@@ -12,7 +15,7 @@ app.get('/jade', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(server_port, server_ip_address, function () {
   var host = server.address().address;
   var port = server.address().port;
 
